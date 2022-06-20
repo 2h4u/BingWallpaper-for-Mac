f135e2f19260e4da00918dec599d0e142c076577
Laurenz Lazarus <laurenz@lazarus-awk.at>
2022-06-20 07:26:05 +0200

    Initial Commit

---
+++ BingWallpaper/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  BingWallpaper
+//
+//  Created by Laurenz Lazarus on 20.06.22.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+  
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+  func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+    return true
+  }
+
+
+}
+
---
+++ BingWallpaper/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  BingWallpaper
+//
+//  Created by Laurenz Lazarus on 20.06.22.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
